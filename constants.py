b23676bbd675dc53d1fc7b26aee0c25c985882b3
zaidalyafeai <alyafey22@gmail.com>
2025-01-07 23:54:51 +0300

    add valid symbol names for dataset verification

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+VALID_CHARS_NAMES = 'abcdefghijklmnopqrstuvwxyz0123456789'
+VALID_PUNCT_NAMES = '-_&.:()[] '
+VALID_SYMP_NAMES = VALID_CHARS_NAMES + VALID_PUNCT_NAMES
+
 HF_REQUEST_BATCH_SIZE = 200
 
 HF_API_URL = 'https://api-inference.huggingface.co'
